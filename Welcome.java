1f3b69d6c44ea080c6504a94f36187d481199bbe
pworrins <159430413+pworrins@users.noreply.github.com>
2024-02-08 22:11:33 +0700

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -14,7 +14,7 @@ public class Welcome {
         String greeting = "Welcome to Core Java!";
             System.out.println(greeting);
             for (int i =0; i< greeting.length(); i++)
-                System.out.println("=");
+                System.out.print("=");
             System.out.println();
     
     }
