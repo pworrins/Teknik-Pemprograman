4eed6c147bfa50f746b4ce1f4a10a1f8119203e1
pworrins <159430413+pworrins@users.noreply.github.com>
2024-02-08 17:28:43 +0700

    Add files via upload
    Pertemuan 1 praktikum teknik pemprograman

---
+++ Soal2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.welcome;
+
+/**
+ *
+ * @author USER
+ */
+public class Soal2 {
+    public static void main(String[] args) {
+        int i = 42;
+        String s = (i<40)?"life":(i>50)?"universe":"everything";
+        System.out.println(s);
+     }
+}
---
+++ Welcome.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.welcome;
+
+/**
+ *
+ * @author USER
+ */
+public class Welcome {
+
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java!";
+            System.out.println(greeting);
+            for (int i =0; i< greeting.length(); i++)
+                System.out.println("=");
+            System.out.println();
+    
+    }
+}
